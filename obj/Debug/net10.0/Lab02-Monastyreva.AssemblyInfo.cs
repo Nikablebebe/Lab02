7b24a5892f3ec74f4c8c335878848021bf974588
Nikablebebe <nestarberry3@gmail.com>
2026-09-14 21:35:07 +0300

    Lab02: keyboard input with ReadLine

--- obj/Debug/net10.0/Lab02-Monastyreva.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Monastyreva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Monastyreva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d169ac384a42a72849be4a4af2129dfdbe08f38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4081cb39a62fb1f4d05ae98a346f1dd385a517af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Monastyreva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Monastyreva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
