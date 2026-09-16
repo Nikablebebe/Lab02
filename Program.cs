308f18878c1b1f38952692d5be36cbff73fe52e3
Nikablebebe <nestarberry3@gmail.com>
2026-09-16 20:30:26 +0300

    Lab02 final: student questionnarie with input

--- Program.cs
+++ Program.cs
@@ -39,6 +39,7 @@ decimal priceDecimal = 0.1m + 0.2m;
 Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
 Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
 
+
 Console.WriteLine();
 Console.WriteLine("var");
 
@@ -47,6 +48,7 @@ var gpa = 4.75; // компилятор вывел double
 var fullName = "Смирнова А.C."; // компилятор вывел string
 
 Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
+
 
 Console.WriteLine();
 Console.WriteLine("Ввод текста");
@@ -58,6 +60,7 @@ Console.Write("Введите название вашей группы: ");
 string enteredGroup = Console.ReadLine();
 
 Console.WriteLine($"Здравствуйте, {enteredName} из группы {enteredGroup}!");
+
 
 Console.WriteLine();
 Console.WriteLine("Ввод чисел: Convert и Parse");
@@ -82,3 +85,35 @@ bool wasSuccessful = int.TryParse(bookInput, out int booksCount);
 
 Console.WriteLine($"Удалось преобразовать: {wasSuccessful}");
 Console.WriteLine($"Значение переменноц booksCount: {booksCount}");
+
+Console.WriteLine();
+Console.WriteLine("Введите имя и фамилию:");
+string fullname = Console.ReadLine();
+
+Console.WriteLine("Введите группу:");
+string groupName = Console.ReadLine();
+
+Console.WriteLine("Введите год рождения:");
+string yearInput = Console.ReadLine();
+bool yearLSK = int.TryParse(yearInput, out int birthYear);
+
+Console.WriteLine("Введите средний балл:");
+string gradeInput = Console.ReadLine();
+bool gradeLKJ = double.TryParse(gradeInput, out double averageGrade);
+
+Console.WriteLine("Введите любимую букву:");
+string letterInput = Console.ReadLine();
+char favoritelEtter = letterInput[0];
+
+int age2030 = 2030 - birthYear;
+
+bool isGoodGrade = averageGrade >= 4.0;
+
+Console.WriteLine();
+Console.WriteLine("    Анкета");
+Console.WriteLine($"{fullname}, группа {groupName}");
+Console.WriteLine($"Год рождения: {birthYear} (в 2030 будкт {age2030} лет)");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Балл >= 4.0: {isGoodGrade}");
+Console.WriteLine($"Любимая буква: {favoritelEtter}");
+
--- obj/Debug/net10.0/Lab02-Monastyreva.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Monastyreva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Monastyreva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b24a5892f3ec74f4c8c335878848021bf974588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c160207486729ced04daf2c78b476c1030762a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Monastyreva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Monastyreva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
